be3ee06c5192b2c333cbedcd8dedfec49a883279
Fabio Costa <fabiobc3@gmail.com>
2021-02-18 16:04:10 -0500

    othello page in json

--- main.js
+++ main.js
@@ -6,3 +6,47 @@ fetch('data.json')
   console.log(data);
 	// render HTML here
 });
+
+// function renderNavbar(page){}
+// function renderAbout(about){}
+// function renderNews(news){}
+
+// function renderMainPage(data){
+
+//     document.querySelector('.container').innerHTML = `
+//         ${renderNavbar('main', Object.keys(data))}
+//         ${renderAbout(data.about)}
+//         ${renderNews(data.news)}
+//         ${renderProjects(data.projects)}
+//     `;
+// }
+
+// function renderProjects(projects){
+//     return `
+//     <section id="projects">
+//         <h1 class="title">Projects</h1>
+//         <!-- we will add a filter interface here in the next lab -->
+//         <div class="project-list">
+//             ${renderProjectItems(projects)}
+//         </div>
+//     </section>`;
+// }
+
+
+// function renderProjectItems(projects){
+// 	return projects.map(d=>`
+// 	  <div class="row">
+//       <div class="col-6">
+//         <div class="project-title">
+//           <a href="?project=${d.id}"><strong>${d.title}</strong></a>
+//         </div>
+//         <div class="project-authors">
+//           ${d.authors}<br>
+//         </div>
+// 				...
+// 			<div class="col-6">
+//         <img src="${d.teaser}" width="100%">
+//       </div>
+// 		</div>
+// 	`).join('');
+// }
